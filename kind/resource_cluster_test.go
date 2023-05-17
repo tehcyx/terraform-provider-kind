aa8a9f35e74fdcdd620dd9c7f9810f9efb5d8bc6
Daniel Roth <daniel.broetchen@gmail.com>
2023-05-17 08:08:41 -0700

    fix cluster resource test

--- kind/resource_cluster_test.go
+++ kind/resource_cluster_test.go
@@ -177,8 +177,8 @@ func TestAccClusterConfigBase(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "kind_config.0.kind", "Cluster"),
 					resource.TestCheckResourceAttr(resourceName, "kind_config.0.api_version", "kind.x-k8s.io/v1alpha4"),
 					resource.TestCheckResourceAttr(resourceName, "kind_config.0.runtime_config.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "kind_config.0.runtime_config.0", "api/all"),
-					resource.TestCheckResourceAttr(resourceName, "kind_config.0.runtime_config.0.api/all", "false"),
+					resource.TestCheckResourceAttr(resourceName, "kind_config.0.runtime_config.0", "api/alpha"),
+					resource.TestCheckResourceAttr(resourceName, "kind_config.0.runtime_config.0.api/alpha", "false"),
 				),
 			},
 			{
@@ -734,7 +734,7 @@ resource "kind_cluster" "test" {
 	api_version = "kind.x-k8s.io/v1alpha4"
 
 	runtime_config {
-		"api/alpha": "false"
+		"api/alpha" = "false"
 	}
   }
 }
